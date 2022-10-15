a223728e3f97f0c2248f70275ec9c9bdf1665840
eyal12345 <eyal1236@gmail.com>
2022-10-16 01:09:14 +0300

    first try

--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,90 @@
-public class Queue {
+public class Queue<T extends Comparable<? super T>> {
+    private final T[] arr;
+    private int front;
+    private int rear;
+    private final int size;
+    private int count;
+
+    public Queue(int size) {
+        arr = (T[])new Comparable[size];
+        this.size = size;
+        this.front = 0;
+        this.rear = -1;
+        this.count = 0;
+    }
+
+    public void enqueue(T val) {
+        if (isFull()) {
+            System.out.println("the queue is full");
+        } else {
+            System.out.println("inserted " + val + " value");
+            rear = (rear + 1) % size;
+            arr[rear] = val;
+            count++;
+        }
+    }
+    public T dequeue() {
+        if (isEmpty()) {
+            System.out.println("the queue is empty");
+            return null;
+        }
+        System.out.println("removed " + peek() + " value");
+        T x = arr[front];
+        front = (front + 1) % size;
+        count--;
+        return x;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        if (isFull()) {
+            return arr[size - 1];
+        }
+        return arr[front];
+    }
+
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
+
+    public boolean isFull() {
+        return (size() == size);
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void printQueue() {
+        if (isEmpty()) {
+            System.out.println("the queue is empty");
+        } else {
+            for (int i = 0 ;i < count ;i++) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new Queue<Integer>(6);
+        boolean f = q.isEmpty();
+        System.out.println(f);
+        q.dequeue();
+        q.enqueue(4);
+        q.enqueue(4);
+        q.enqueue(4);
+        q.enqueue(4);
+        q.enqueue(4);
+        q.enqueue(4);
+        q.printQueue();
+        q.enqueue(4);
+        q.dequeue();
+        f = q.isFull();
+        System.out.println(f);
+        q.printQueue();
+        int l = q.size();
+        System.out.println(l);
+    }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,84 @@
-public class Stack {
+public class Stack<T extends Comparable<? super T>> {
+    private final T[] arr;
+    private final int size;
+    private int count;
+
+    public Stack(int size) {
+        arr = (T[])new Comparable[size];
+        this.size = size;
+        this.count = 0;
+    }
+
+    public void push(T val) {
+        if (isFull()) {
+            System.out.println("the stack is full");
+        } else {
+            System.out.println("inserted " + val + " value");
+            arr[count] = val;
+            count++;
+        }
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("the stack is empty");
+            return null;
+        }
+        System.out.println("removed " + peek() + " value");
+        count--;
+        return arr[count];
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        if (isFull()) {
+            return arr[size - 1];
+        }
+        return arr[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return (size() == 0);
+    }
+
+    public boolean isFull() {
+        return (size() == size);
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public void printStack() {
+        if (isEmpty()) {
+            System.out.println("the stack is empty");
+        } else {
+            for (int i = 0 ;i < count ;i++) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<Integer>(6);
+        boolean f = s.isEmpty();
+        System.out.println(f);
+        s.pop();
+        s.push(4);
+        s.push(4);
+        s.push(4);
+        s.push(4);
+        s.push(4);
+        s.push(4);
+        s.printStack();
+        s.push(4);
+        s.pop();
+        f = s.isFull();
+        System.out.println(f);
+        s.printStack();
+        int l = s.size();
+        System.out.println(l);
+    }
 }
